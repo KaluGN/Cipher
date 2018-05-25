88fef1ba5bfdfaec891c34d26bccaaceccf9a5dd
KaluGN <kaluguzmann@gmail.com>
2018-05-25 08:39:46 -0500

    java

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function cifrar() {
+    const saltos = document.getElementById("desplazar").value;
+    console.log(saltos);
+
+    const texto_Original = document.getElementById("mensaje").value;
+    console.log(texto_Original);
+
+
+}
