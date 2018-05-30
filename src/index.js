76ca6fc36decd63f27c371b01f31ebd80c786571
KaluGN <kaluguzmann@gmail.com>
2018-05-30 09:42:48 -0500

    ya cifra en mayusculas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-function cifrar() {
-    const saltos = document.getElementById("desplazar").value;
-    console.log(saltos);
+// declarar variables//
+const numero = document.getElementById("saltos");
+const texto = document.getElementById("string");
+const boton1 = document.getElementById("boton1");
+const boton2 = document.getElementById("boton2");
+const output = document.getElementById("output");
 
-    const texto_Original = document.getElementById("mensaje").value;
-    console.log(texto_Original);
+//funcion de cifrado
+function encode (offset,string) {
+   let output = "";
+   const clave = parseInt(offset);
+      for (i=0; i<string.length; i++){
+      let formula = (string.charCodeAt(i) - 65 + clave)%26 + 65;
+        output += String.fromCharCode(formula);
+     }  return output;
+     }
 
-
-}
+   const btnCifrar = () => {
+        console.log('hola');
+        console.log(encode(numero.value,texto.value));
+        output.value = encode(numero.value,texto.value);
+   } 
+   boton1.addEventListener("click", btnCifrar);
