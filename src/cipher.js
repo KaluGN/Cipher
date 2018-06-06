8186e188b2beffc608dd7a8388d89b07f0d4e697
KaluGN <kaluguzmann@gmail.com>
2018-06-06 09:40:32 -0500

    cifrado sin espacio

--- src/cipher.js
+++ src/cipher.js
@@ -2,18 +2,24 @@ window.cipher = {
   encode: (string,offset) => {
     let output = "";
     const clave = parseInt(offset);
-      for (let i=0; i<string.length; i++){
-    let formula = (string.charCodeAt(i) - 65 + clave) % 26 + 65;
-      output += String.fromCharCode(formula);
-    } return output;
+    for (let i=0; i<string.length; i++){
+      if ( string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        let formula = (string.charCodeAt(i) - 65 + clave) % 26 + 65;
+        output += String.fromCharCode(formula);
+      }
+    } 
+    return output;
   },
 
   decode: (string,offset) => {
     let output = "";
     const clave = parseInt(offset);
-      for( let i=0;i<string.length;i++){
-    let formula = (string.charCodeAt(i) - 90 - clave) % 26 + 90;
-      output += String.fromCharCode(formula);
-    } return output;
+    for( let i=0;i<string.length;i++){
+      if ( string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        let formula = (string.charCodeAt(i) - 90 - clave) % 26 + 90;
+        output += String.fromCharCode(formula);
+      }
+    } 
+    return output;
   }
 };
